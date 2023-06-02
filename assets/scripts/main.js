4eafb168f76a714777e1e5c92e84f8718404f7f1
Mesh <harmeshuppal@gmail.com>
2023-06-02 20:31:32 +0100

    Remove v1 files

--- assets/scripts/main.js
+++
@@ -1,6 +0,0 @@
-let reCaptcha;
-
-window.addEventListener('DOMContentLoaded', (event) => {
-  const googleSiteKey = '6LfRZrUhAAAAAJ5O2p0hgsE0uFStSIeOz1aemopm';
-  reCaptcha = new ReCaptcha(grecaptcha, googleSiteKey);
-});
