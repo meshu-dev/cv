97719ffa1c6aa89a7fd98a4935c880a1a0053455
Mesh <harmeshuppal@gmail.com>
2023-10-27 00:30:38 +0100

    Update test endpoint

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -1,3 +1,17 @@
-export default function handler(req, res) {
-  res.status(200).json({ text: 'Hello' });
+
+export default (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      res.json({ method: 'GET', endpoint: 'Users' });
+      break;
+    case 'POST':
+      res.json({ method: 'POST', endpoint: 'Users' });
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
 }
