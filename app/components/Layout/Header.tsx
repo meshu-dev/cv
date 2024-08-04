e1c68affbcb5f5ba1c2d71e05d4b05d91d7707d3
Mesh <harmeshuppal@gmail.com>
2024-08-04 19:22:08 +0100

    Fix merge issue

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -9,35 +9,31 @@ type Props = {
 }
 
 const Header = ({ pdfUrl }: Props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const googleKey: string = process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY || '';
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const googleKey: string = process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY || ''
 
-  const Header = ({ cv }: Props) => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const googleKey: string = process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_KEY || ''
+  let googleRecaptcha: React.ReactElement = <></>
 
-    let googleRecaptcha: React.ReactElement = <></>
-
-    if (googleKey) {
-      googleRecaptcha = <GoogleReCaptchaProvider reCaptchaKey={googleKey}>
-        <ContactForm isOpen={isOpen} onClose={onClose} />
-      </GoogleReCaptchaProvider>
-    }
-
-    return (
-      <>
-        <header>
-          <div id="header-wrapper">
-            <div id="header-logo">Mesh's CV</div>
-            <div id="header-links">
-              {pdfUrl ? <a href={pdfUrl} target="_blank" className="header-link">Download</a> : null}
-              <span className="header-link" onClick={onOpen}>Contact</span>
-            </div>
-          </div>
-        </header>
-        {googleRecaptcha}
-      </>
-    )
+  if (googleKey) {
+    googleRecaptcha = <GoogleReCaptchaProvider reCaptchaKey={googleKey}>
+      <ContactForm isOpen={isOpen} onClose={onClose} />
+    </GoogleReCaptchaProvider>
   }
 
-  export default Header
+  return (
+    <>
+      <header>
+        <div id="header-wrapper">
+          <div id="header-logo">Mesh's CV</div>
+          <div id="header-links">
+            {pdfUrl ? <a href={pdfUrl} target="_blank" className="header-link">Download</a> : null}
+            <span className="header-link" onClick={onOpen}>Contact</span>
+          </div>
+        </div>
+      </header>
+      {googleRecaptcha}
+    </>
+  )
+}
+
+export default Header
